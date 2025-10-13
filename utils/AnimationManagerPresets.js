fd97d06f28e2e66b3aa1528549d03ef677285f2b
chnak <chnak@163.com>
2025-10-14 01:05:56 +0800

    xcxc

--- utils/AnimationManagerPresets.js
+++ utils/AnimationManagerPresets.js
@@ -216,9 +216,9 @@ export const AnimationManagerPresets = {
 
   // 故障效果
   glitch: [
-    { property: 'x', from: 0, to: -5, duration: 0.1, easing: 'linear' , isOffset: true},
-    { property: 'x', from: -5, to: 5, duration: 0.1, easing: 'linear', delay: 0.1 , isOffset: true},
-    { property: 'x', from: 5, to: 0, duration: 0.1, easing: 'linear', delay: 0.2 , isOffset: true},
+    { property: 'x', from: 0, to: -10, duration: 0.1, easing: 'linear' , isOffset: true},
+    { property: 'x', from: -10, to: 10, duration: 0.1, easing: 'linear', delay: 0.1 , isOffset: true},
+    { property: 'x', from: 10, to: 0, duration: 0.1, easing: 'linear', delay: 0.2 , isOffset: true},
     { property: 'opacity', from: 1, to: 0.8, duration: 0.1, easing: 'linear', delay: 0.1 },
     { property: 'opacity', from: 0.8, to: 1, duration: 0.1, easing: 'linear', delay: 0.2 }
   ],
